e0bf74b8daee91434ae0ba92d8b48ae22fbad155
Switch-modder <deolrajjyot@gmail.com>
2025-08-10 15:11:25 -0400

    Old intent graph

--- pkg/preqs/intent_graph.go
+++ pkg/preqs/intent_graph.go
@@ -4,22 +4,23 @@ import (
 	"fmt"
 	"strings"
 
+	pb "github.com/digital-dream-labs/api/go/chipperpb"
+	"cavalier/pkg/vars"
 	"cavalier/pkg/vtt"
-
 	sr "cavalier/pkg/speechrequest"
 	ttr "cavalier/pkg/ttr"
-	"cavalier/pkg/vars"
 )
 
 func (s *Server) ProcessIntentGraph(req *vtt.IntentGraphRequest) (*vtt.IntentGraphResponse, error) {
 	var successMatched bool
 	speechReq := sr.ReqToSpeechRequest(req)
 	var transcribedText string
+	
 	if !isSti {
 		var err error
 		transcribedText, err = sttHandler(speechReq)
 		if err != nil {
-			ttr.IntentPass(req, "intent_system_noaudio", "voice processing error: "+err.Error(), map[string]string{"error": err.Error()}, true)
+			ttr.IntentPass(req, "intent_system_noaudio", "voice processing error", map[string]string{"error": err.Error()}, true)
 			return nil, nil
 		}
 		if strings.TrimSpace(transcribedText) == "" {
@@ -42,24 +43,43 @@ func (s *Server) ProcessIntentGraph(req *vtt.IntentGraphRequest) (*vtt.IntentGra
 		ttr.ParamCheckerSlotsEnUS(req, intent, slots, speechReq.IsOpus, speechReq.Device)
 		return nil, nil
 	}
+	
 	if !successMatched {
-		// 	fmt.Println("No intent was matched.")
-		// 	if vars.APIConfig.Knowledge.Enable && vars.APIConfig.Knowledge.Provider == "openai" && len([]rune(transcribedText)) >= 8 {
-		// 		apiResponse := openaiRequest(transcribedText)
-		// 		response := &pb.IntentGraphResponse{
-		// 			Session:      req.Session,
-		// 			DeviceId:     req.Device,
-		// 			ResponseType: pb.IntentGraphMode_KNOWLEDGE_GRAPH,
-		// 			SpokenText:   apiResponse,
-		// 			QueryText:    transcribedText,
-		// 			IsFinal:      true,
-		// 		}
-		// 		req.Stream.Send(response)
-		// 		return nil, nil
-		// 	}
+		fmt.Println("No intent was matched.")
+		
+		if vars.APIConfig.Knowledge.Enable && vars.APIConfig.Knowledge.Provider == "houndify" && len([]rune(transcribedText)) >= 8 {
+			fmt.Println("Making Houndify request for device " + req.Device + "...")
+			
+			InitKnowledge()
+			
+			apiResponse := houndifyKG(speechReq)
+			
+			if apiResponse != "" && apiResponse != "Houndify is not enabled." {
+				response := &pb.IntentGraphResponse{
+					Session:      req.Session,
+					DeviceId:     req.Device,
+					ResponseType: pb.IntentGraphMode_KNOWLEDGE_GRAPH,
+					SpokenText:   apiResponse,
+					QueryText:    transcribedText,
+					IsFinal:      true,
+				}
+				
+				if err := req.Stream.Send(response); err != nil {
+					fmt.Println("Error sending IntentGraph response:", err)
+					return nil, err
+				}
+				
+				fmt.Println("Bot " + req.Device + " Houndify IntentGraph request served.")
+				return nil, nil
+			} else {
+				fmt.Println("No valid response from Houndify")
+			}
+		}
+		
 		ttr.IntentPass(req, "intent_system_unmatched", transcribedText, map[string]string{"": ""}, false)
 		return nil, nil
 	}
+	
 	fmt.Println("Bot " + speechReq.Device + " request served.")
 	return nil, nil
 }
